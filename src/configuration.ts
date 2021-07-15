bf3744d2ab09ca9af72afe057db6a4fb28a9c0b5
Manuel Puyol <manuelpuyol@github.com>
2021-07-15 12:25:50 -0500

    lint all files

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,12 +1,12 @@
-import * as vs from "vscode";
-import { ERBLint } from "./erbLint";
+import {workspace} from 'vscode'
+import {ERBLint} from './erbLint'
 
 export interface ERBLintConfig {
-  command: string;
-  onSave: boolean;
-  configFilePath: string;
-  suppressERBLintWarnings: boolean;
-  executePath: string;
+  command: string
+  onSave: boolean
+  configFilePath: string
+  suppressERBLintWarnings: boolean
+  executePath: string
 }
 
 /**
@@ -14,30 +14,28 @@ export interface ERBLintConfig {
  * @return {ERBLintConfig} config object
  */
 export const getConfig: () => ERBLintConfig = () => {
-  const cmd = "erblint";
-  const conf = vs.workspace.getConfiguration("erb.erb-lint");
-  let configPath = conf.get("executePath", "");
-  let suppressERBLintWarnings = conf.get("suppressERBLintWarnings", true);
-  let command;
+  const cmd = 'erblint'
+  const conf = workspace.getConfiguration('erb.erb-lint')
+  const configPath = conf.get('executePath', '')
+  const suppressERBLintWarnings = conf.get('suppressERBLintWarnings', true)
+  let command
 
   // if executePath is present in workspace config, use it.
   if (configPath.length !== 0) {
-    command = configPath + cmd;
+    command = configPath + cmd
   } else {
-    command = `bundle exec ${cmd}`;
+    command = `bundle exec ${cmd}`
   }
 
   return {
     command,
-    configFilePath: conf.get("configFilePath", ""),
-    onSave: conf.get("onSave", true),
+    configFilePath: conf.get('configFilePath', ''),
+    onSave: conf.get('onSave', true),
     suppressERBLintWarnings,
-    executePath: configPath,
-  };
-};
+    executePath: configPath
+  }
+}
 
-export const onDidChangeConfiguration: (erbLint: ERBLint) => () => void = (
-  erbLint
-) => {
-  return () => (erbLint.config = getConfig());
-};
+export const onDidChangeConfiguration: (erbLint: ERBLint) => () => void = erbLint => {
+  return () => (erbLint.config = getConfig())
+}
--- src/erbLintOutput.ts
+++ src/erbLintOutput.ts
@@ -1,39 +1,39 @@
 // output of erb-lint JSON format
 interface ERBLintSummary {
-  offenses: number;
-  target_files: number;
-  corrected: number;
+  offenses: number
+  target_files: number
+  corrected: number
 }
 
 interface ERBLintLocation {
-  start_line: number;
-  start_column: number;
-  last_line: number;
-  last_column: number;
-  length: number;
+  start_line: number
+  start_column: number
+  last_line: number
+  last_column: number
+  length: number
 }
 
 export interface ERBLintOffense {
-  message: string;
-  linter: string;
-  location: ERBLintLocation;
+  message: string
+  linter: string
+  location: ERBLintLocation
 }
 
 export interface ERBLintFile {
-  path: string;
-  offenses: Array<ERBLintOffense>;
+  path: string
+  offenses: ERBLintOffense[]
 }
 
 interface ERBLintMetadata {
-  erb_lint_version: string;
-  ruby_engine: string;
-  ruby_version: string;
-  ruby_patchlevel: string;
-  ruby_platform: string;
+  erb_lint_version: string
+  ruby_engine: string
+  ruby_version: string
+  ruby_patchlevel: string
+  ruby_platform: string
 }
 
 export interface ERBLintOutput {
-  metadata: ERBLintMetadata;
-  files: Array<ERBLintFile>;
-  summary: ERBLintSummary;
+  metadata: ERBLintMetadata
+  files: ERBLintFile[]
+  summary: ERBLintSummary
 }
